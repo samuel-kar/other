1517de998d2321598be4dd397f8b8d8c879d484b
samuel-kar <samuel.karlhager@hotmail.com>
2025-03-29 18:04:25 +0100

    adds throwing to example-file

--- more_repetition/ExceptionExample.java
+++ more_repetition/ExceptionExample.java
@@ -1,4 +1,21 @@
 package repetera.objects.more_repetition;
 
 public class ExceptionExample {
+    public static void main(String[] args) {
+        calculateNumber(101,3);
+        try{
+            int a = 42/0;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            System.out.println("You've made a bobo.");
+        } finally {
+            System.out.println("All good in the hood, we're here to learn.");
+        }
+
+    }
+    public static void calculateNumber(int numberOne, int numberTwo){
+        if(numberOne>100){
+            throw new IllegalArgumentException("We do not allow too big numbers here..");
+        }
+    }
 }
